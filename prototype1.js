e8317f8bab3ff54bf4a49f2d4051e9345eb1df86
ZainalFauzi <fourth.breast@gmail.com>
2020-09-03 12:01:25 +0700

    js Prototype done

--- prototype1.js
+++ prototype1.js
@@ -2,7 +2,7 @@
 function reverseString(str) {
     if (str === "")
       return "";
-    else
+    else 
       return reverseString(str.substr(1)) + str.charAt(0);
   }
   reverseString("rotavator");
---
+++ prototype2.js
@@ -0,0 +1,15 @@
+Number.prototype.primeCheck = function (n) {
+  if (n === 1) {
+    return false;
+  } else if (n === 2) {
+    return true;
+  } else {
+    for (var x = 2; x < n; x++) {
+      if (n % x === 0) {
+        return false;
+      }
+    }
+        return true;
+  }
+};
+console.log(Number.prototype.primeCheck(9));
